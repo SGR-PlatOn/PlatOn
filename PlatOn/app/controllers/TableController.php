7bc64c714f23421e234ceed4cc78fd18e3915558
mehdi859 <69049181+mehdi859@users.noreply.github.com>
2025-11-21 08:43:34 +0100

    Fix method name case in TableController

--- PlatOn/app/controllers/TableController.php
+++ PlatOn/app/controllers/TableController.php
@@ -1,17 +1,22 @@
 <?php
 require_once 'C:\xampp\htdocs\PlatOn\config\database.php';
 require_once 'C:\xampp\htdocs\PlatOn\app\models\Table.php';
+
 class TableController{
-    private $db;
-    private $tables;
-    public function __construct(){
-        $database = new Database();
-        $this->db = $database->connect();
-        $this->tables = new tables($this->db);
-    }
-    public function index(){
-        $stmt = $this->tables->readAll();
-        $tables = $stmt->fetchAll(PDO::FETCH_ASSOC);
-        include 'C:\xampp\htdocs\PlatOn\app\views\table\map.php';
-    }
+    private $db;
+    private $tables;
+
+    public function __construct(){
+        $database = new Database();
+        $this->db = $database->connect();
+        $this->tables = new tables($this->db);
+    }
+
+    public function index(){
+        $stmt = $this->tables->ReadAll();
+        $tables = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+        include 'C:\xampp\htdocs\PlatOn\app\views\table\map.php';
+    }
+
 }
